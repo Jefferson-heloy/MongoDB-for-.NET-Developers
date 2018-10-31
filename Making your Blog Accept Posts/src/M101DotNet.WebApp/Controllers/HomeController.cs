2d82b2557fc2ac7ce76c54c4e8782df054374cfb
Jefferson Heloy <jeffersonheloy@gmail.com>
2018-10-31 14:51:10 -0300

    Making your Blog Accept Posts
    In this homework you will be enhancing the blog project to insert entries into the posts collection. After this, the blog will work. It will allow you to add blog posts with a title, content, and tags and have it be added to the posts collection properly.
    
    We have provided the code that creates users and allows you to login (the assignment from last week). To get started, please download the handout and unpack.
    
    The areas where you need to add code are marked with XXX. You need only touch the Post.cs file, the Comments.cs file, and the HomeController.cs file. There are seven locations for you to add code for this problem:
    
    5 in HomeController.cs
    1 in Post.cs
    1 in Comment.cs
    Here is an example of a valid blog post.
    
    > db.posts.find().pretty()
    {
            "_id" : ObjectId("54c298ec122ea911588b7970"),
            "Author" : "Craig",
            "Title" : "Frist!!!",
            "Content" : "This is my first post!!!",
            "Tags" : [
                    "first",
                    "awesome"
            ],
            "CreatedAtUtc" : ISODate("2015-01-23T18:54:36.335Z"),
            "Comments" : [
                    {
                            "Author" : "Jack",
                            "Content" : "This is a comment.",
                            "CreatedAtUtc" : ISODate("2015-01-23T18:54:42.005Z")
                    }
            ]
    }
    We are going to validate that this worked properly. We will be checking for the following functionality:
    
    Your blog can accept posts.
    Your posts can be tagged.
    Your posts can receive comments from others who are logged in.

---
+++ Making your Blog Accept Posts/src/M101DotNet.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+using M101DotNet.WebApp.Models;
+using M101DotNet.WebApp.Models.Home;
+using System.Linq.Expressions;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Web.Mvc;
+
+namespace M101DotNet.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var blogContext = new BlogContext();
+            // XXX WORK HERE
+            // find the most recent 10 posts and order them
+            // from newest to oldest
+            var filter = new BsonDocument();
+            var recentPosts = await blogContext.Posts.Find(filter).Limit(10).Sort("{Date:1}").ToListAsync();
+
+            var model = new IndexModel
+            {
+                RecentPosts = recentPosts
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult NewPost()
+        {
+            return View(new NewPostModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NewPost(NewPostModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var blogContext = new BlogContext();
+            // XXX WORK HERE
+            // Insert the post into the posts collection
+            var post = new Post
+            {
+                Date = DateTime.UtcNow,
+                Title = model.Title,
+                Content = model.Content,
+                Author = User.Identity.Name
+            };
+            string[] words = model.Tags.Split(',');
+            post.Tags = new List<string>();
+            foreach(string word in words)
+            {
+                post.Tags.Add(word);
+            }
+
+            await blogContext.Posts.InsertOneAsync(post);
+
+            return RedirectToAction("Post", new { id = post.Id });
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Post(string id)
+        {
+            var blogContext = new BlogContext();
+
+            // XXX WORK HERE
+            // Find the post with the given identifier
+            var post = await blogContext.Posts.Find(x => x.Id == ObjectId.Parse(id)).SingleAsync();
+            if (post == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var model = new PostModel
+            {
+                Post = post
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Posts(string tag = null)
+        {
+            var blogContext = new BlogContext();
+
+            // XXX WORK HERE
+            // Find all the posts with the given tag if it exists.
+            // Otherwise, return all the posts.
+            // Each of these results should be in descending order.
+            var filter = new BsonDocument("Tags", tag);
+            var posts = await blogContext.Posts.Find(filter).Limit(10).ToListAsync();
+            return View(posts);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NewComment(NewCommentModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Post", new { id = model.PostId });
+            }
+
+            var blogContext = new BlogContext();
+            // XXX WORK HERE
+            // add a comment to the post identified by model.PostId.
+            // you can get the author from "this.User.Identity.Name"
+
+            var post = new Post();
+            var commentList = new List<Comment>();
+            var comment = new Comment
+            {
+                Data = DateTime.UtcNow,
+                Author = User.Identity.Name,
+                Content = model.Content
+            };
+            commentList.Add(comment);
+
+            post = await blogContext.Posts.Find(x => x.Id == ObjectId.Parse(model.PostId)).SingleAsync();
+            if (post.Comments != null)
+            {
+                post.Comments.Add(comment);
+            }
+            else
+            {
+                post.Comments = commentList;
+            }
+            var result = await blogContext.Posts.ReplaceOneAsync(x => x.Id == ObjectId.Parse(model.PostId), post);
+
+
+            return RedirectToAction("Post", new { id = model.PostId });
+        }
+    }
+}
